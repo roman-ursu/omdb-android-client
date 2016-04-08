7ad0ce82a7a902cd749b99f1b8a48eb4951825c6
rursu <roman.ursu@perfectial.com>
2016-04-08 19:44:10 +0300

    net api code added

---
+++ app/src/main/java/com/perfectial/omdb/net/bean/OpenDBMovieEntity.java
@@ -0,0 +1,14 @@
+package com.perfectial.omdb.net.bean;
+
+/**
+ * Created by rursu on 08.04.16.
+ */
+public class OpenDBMovieEntity {
+
+    private String title;
+    private String year;
+    private String imdbID;
+    private String Type;
+    private String poster;
+
+}
